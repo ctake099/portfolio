979479dcd2a7060f23f4b67cdd71650481805813
ctake <ctake0099@gmail.com>
2024-09-28 20:56:16 +0900

    trailingSlashの追加

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
   },
   basePath: process.env.NEXT_PUBLIC_ASSET_PREFIX,  // .env.localから読み込み
   assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,  // .env.localから読み込み
+  trailingSlash: true, 
   reactStrictMode: true,
 };
 
