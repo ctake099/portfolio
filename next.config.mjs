c7ff07375e26a91a926399ae07c20380ec8a417c
ctake <ctake0099@gmail.com>
2024-09-30 13:34:13 +0900

    url 設定

--- next.config.mjs
+++ next.config.mjs
@@ -7,14 +7,16 @@ const nextConfig = {
         output: 'export',
         distDir: 'docs',
         images: {
-          unoptimized: true,
+          unoptimized: true, // GitHub Pages用に画像最適化を無効
         },
-        basePath: process.env.NEXT_PUBLIC_ASSET_PREFIX,
-        assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
-        trailingSlash: true,
+        basePath: '', // カスタムドメインの場合は空に設定
+        assetPrefix: '', // カスタムドメインを使う場合はassetPrefixも空に
+        trailingSlash: true, // 末尾にスラッシュを付ける
       }
     : {
         // 開発環境の設定
+        basePath: '',
+        assetPrefix: '',
       }),
   reactStrictMode: true,
 };
